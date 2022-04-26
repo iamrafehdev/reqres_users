0de769c153875c7cf0039cd3979f17a4daf23f80
iamrafehdev <abdulrafeh46@gmail.com>
2022-04-26 12:33:37 +0500

    ios setup

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:reqres_users/controllers/auth_controller.dart';
+import 'package:reqres_users/widgets/others/sized_boxes.dart';
+import 'package:shimmer/shimmer.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -11,6 +13,22 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   final AuthController _authController = Get.put(AuthController());
+
+  static const _shimmerGradient = LinearGradient(
+    colors: [
+      Color(0xFFEBEBF4),
+      Color(0xFFF4F4F4),
+      Color(0xFFEBEBF4),
+    ],
+    stops: [
+      0.1,
+      0.3,
+      0.4,
+    ],
+    begin: Alignment(-1.0, -0.3),
+    end: Alignment(1.0, 0.3),
+    tileMode: TileMode.clamp,
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -25,6 +43,20 @@ class _HomeScreenState extends State<HomeScreen> {
             icon: Icon(Icons.logout),
           ),
         ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Shimmer(
+              gradient: _shimmerGradient,
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                height: 200.0,
+                color: Colors.black,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
